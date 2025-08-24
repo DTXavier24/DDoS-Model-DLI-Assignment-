9a267ae12af4609fab503367ce2a785d0c24087c
DTXavier24 <tehyuxuancareer@gmail.com>
2025-08-24 12:20:01 +0800

    Import libraries

--- main.py
+++ main.py
@@ -1,1 +1,41 @@
+import numpy as np
+import pandas as pd
 
+import io
+
+import pickle # saving and loading trained model
+
+# importing required libraries for normalizing data
+from sklearn import preprocessing
+from sklearn.preprocessing import StandardScaler
+
+# importing library for plotting
+import matplotlib.pyplot as plt
+import seaborn as sn
+
+from sklearn import metrics
+from sklearn.metrics import accuracy_score # for calculating accuracy of model
+from sklearn.model_selection import train_test_split # for splitting the dataset for training and testing
+from sklearn.metrics import classification_report # for generating a classification report of model
+
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import confusion_matrix
+
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_curve, auc
+
+import tensorflow as tf
+from tensorflow import keras
+
+from keras.layers import Dropout, Activation
+from keras.layers import Dense # importing dense layer
+from keras.models import Sequential #importing Sequential layer
+from keras.models import model_from_json # saving and loading trained model
+
+from keras.layers import LSTM
+from keras.layers import Input
+from keras.models import Model
+
+from tensorflow.keras.utils import plot_model
